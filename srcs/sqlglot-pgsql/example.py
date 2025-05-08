d98d43b08a00366f26d7cff68451fedf01f11d0d
mrp <mrp20021229@163.com>
2025-05-08 17:49:29 +0800

    85549

--- srcs/sqlglot-pgsql/example.py
+++ srcs/sqlglot-pgsql/example.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
-
+import os
+import subprocess
 import random
-import sqlglot_pgsql
+import sqlglot_mysql
 import sqlglot
+
+
 def init(seed):
     pass
+    # try:
+    #     with open("/home/database.txt", "w") as f:
+    #         f.write("1")
+    #     print("[mutator] init: wrote 1 to /home/database.txt")
+    # except Exception as e:
+    #     print(f"[mutator] init: failed to write to file - {e}")
+    # return 0
+
 
 def deinit():
     pass
+
 
 # def deinit():  # optional for Python
 #     passs
@@ -20,27 +32,37 @@ def fuzz_count(buf):
 
 
 def fuzz(buf, add_buf, max_size):
+
+    log_path = "/home/output/fuzz_log.txt"
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
+    #
+    with open(log_path, "a", encoding="utf-8") as log_file:
+        log_file.write("[Original buf]:")
+        try:
+            log_file.write(buf.decode('utf-8') + "\n")
+        except UnicodeDecodeError:
+            log_file.write("[Decode Error:UTF-8]\n")
+
+
     buf = buf.decode('utf-8')
     sql_statements = buf.split(';')
-    
-    
+
     mutated_sql_statements = []
-    
 
     for sql in sql_statements:
-        if sql.strip():  
+        if sql.strip():
 
             mutated_out = None
-            
 
             try:
-                mutated_out = sqlglot_pgsql.mutation(sql.strip())
+                mutated_out = sqlglot_mysql.mutation(sql.strip())
 
             except Exception as e:
                 mutated_out = None
             else:
                 pass
-            
+
             if mutated_out is not None:
                 mutated_sql_statements.append(mutated_out)
                 # print("SDFSDFsdf")
@@ -48,29 +70,30 @@ def fuzz(buf, add_buf, max_size):
                 # with open("/home/mutated_sql.txt", "a") as file:
                 #     file.write("sql: " + sql + "\n")
                 #     file.write("new_sql: " + mutated_out + "\n")
-            else:
-                return bytearray(b'0')
+
 
     # print("SD")
-    
+
     mutated_sql = '; '.join(mutated_sql_statements)
     mutated_sql = mutated_sql.replace('\ufffd', '[INV]')
     mutated_sql = mutated_sql.encode('utf-8', errors='ignore')
     buf = mutated_sql
     buf = bytearray(buf)
+
+    #
+    with open(log_path, "a", encoding="utf-8") as log_file:
+        log_file.write("[Mutated buf]:")
+        try:
+            log_file.write(buf.decode('utf-8') + "\n")
+        except UnicodeDecodeError:
+            log_file.write("[Decode Error:UTF-8]\n")
     if len(buf) == 0:
         return bytearray(b'0')
     return buf
-def strToBytearray(sql):
-    byte = sql.encode('utf-8')
-    byteArray = bytearray(byte)
-    return byteArray
+
 if __name__ == "__main__":
     print("@#@#")
-    sql = "select a from b where c = 3;"
-    tmp = strToBytearray(sql)
-    tnt = fuzz(tmp,None,None)
-    if tnt is None:
-        print("is None")
-    else:
-        print(repr(tnt))
+    sql = "select a from b where c = 3;select c from a;"
+    tmp=strToBytearray(sql)
+    print(repr)
+    print(fuzz(tmp,None,None))
--- srcs/sqlglot-pgsql/sqlglot_pgsql.py
+++ srcs/sqlglot-pgsql/sqlglot_pgsql.py
@@ -5,9 +5,18 @@ import sqlglot_manager
 import sqlglot_mutation
 import sqlglot_fill
 import sys
+import os
 
 def mutation(sql):
+    log_path = "/home/output/fuzz_log.txt"
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
     mutated_sql = sqlglot_mutation.get_mutated_sql(sql)
+    with open(log_path, "a", encoding="utf-8") as log_file:
+        log_file.write("[Mutated SQL Before Fill]")
+        try:
+            log_file.write(mutated_sql + "\n")
+        except Exception as e:
+            log_file.write(f"[Error writing mutated SQL: {e}]\n")
     # print("mutation")
     # print(mutated_sql)
     filled_sql = sqlglot_fill.fill_sql(mutated_sql)
@@ -16,6 +25,16 @@ def mutation(sql):
     return filled_sql
 
 if __name__ == "__main__":
+    sql = """
+    SELECT c2, c6 FROM t1, t2 WHERE t1.c1 = t2.c5;
+    SELECT x FROM x WHERE x > 0.5
+    """
+    print(type(sql))
+    print(sql)
+    byte= sql.encode('utf-8')
+    print(type(byte))
+    print(repr(byte))
+    byte = bytearray(byte)
+    print(repr(byte))
 
-    
-    print(mutation("create table v0(v1 INT, v2 INT);"))
+    # print(mutation("create table v0(v1 INT, v2 INT);"))
